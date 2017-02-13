22258ec79d7a273c47f8af125c4a98ea4d6c87b9
Victoria Guevara <Guevara-chan@mail.ru>
2017-02-13 10:04:16 +0300

    Add files via upload

--- src/builder.cmd
+++ src/builder.cmd
@@ -5,7 +5,7 @@ if exist %tmp_exe% ( del %tmp_exe% )
 C:/Utils/[dot]Net/Boo/bin/booc.exe -debug- -embedres:inc_btn.wav,inc_sound -embedres:dec_btn.wav,dec_sound -embedres:null_btn.wav,null_sound Click.Play.boo
 
 if exist %tmp_exe% (
-C:/Utils/Misc/ResHacker/ResourceHacker.exe -addoverwrite %tmp_exe%, %tmp_exe%, Counter.ico, ICONGROUP, MAINICON, 0
+C:/Utils/Misc/ResHacker/ResourceHacker.exe -open %tmp_exe% -save %tmp_exe% -action addoverwrite -resource Counter.ico -mask ICONGROUP, MAINICON, 0
 C:/Utils/[dot]Net/Extra/ILMerge/ilmerge.exe %tmp_exe% Boo.Lang.dll /t:winexe /ndebug /out:..\[Click.Play].exe
 del %tmp_exe%
 ..\[Click.Play].exe
