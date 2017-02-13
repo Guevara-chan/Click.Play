3e356046cdb2b6f3e4b7f547fa81565239999341
Victoria Guevara <Guevara-chan@mail.ru>
2017-02-13 17:36:24 +0300

    Add files via upload

---
+++ src/release.cmd
@@ -0,0 +1,5 @@
+setlocal
+set release_exe	= ..\[Click.Play].exe
+set res_hacker	= C:/Utils/Misc/ResHacker/ResourceHacker.exe
+set ilmerge		= C:/Utils/[dot]Net/Extra/ILMerge/ilmerge.exe
+call builder.cmd
